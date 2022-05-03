bdc5f5b4a00f56185c36096461c6840694378ac2
WOOGUGU <woogugu@outlook.com>
2022-05-03 22:41:45 +0800

    添加工单请求方法

--- src/api/order.ts
+++ src/api/order.ts
@@ -1,8 +1,27 @@
 import request from "./request";
 
+// 待处理
 export const reviewOrderList = async () => {
   var res = await request({
     url: "/administer/orderlist",
+    method: "get",
+  });
+  return res;
+};
+
+// 待维修
+export const repairOrder = async () => {
+  var res = await request({
+    url: "/administer/orderlist2",
+    method: "get",
+  });
+  return res;
+};
+
+// 已完成
+export const completeOrder = async () => {
+  var res = await request({
+    url: "/administer/orderlist3",
     method: "get",
   });
   return res;
