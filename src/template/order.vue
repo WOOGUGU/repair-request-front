edf2f40959ead0a24f786c1d85c846b1b6c24278
WOOGUGU <woogugu@outlook.com>
2022-05-23 15:01:17 +0800

    限制工单可修改性

--- src/template/order.vue
+++ src/template/order.vue
@@ -8,10 +8,12 @@
                     </div>
                 </template>
                 <el-form-item label="工单号">
-                    <el-input v-model="orderTable.id" placeholder="Please input" />
+                    <el-input class=input v-model="orderTable.id" placeholder="Please input"
+                        :disabled="orderDisabled.orderInformation" />
                 </el-form-item>
                 <el-form-item label="工单类型">
-                    <el-select v-model="orderTable.type" placeholder="please select your zone">
+                    <el-select class=select v-model="orderTable.type" placeholder="please select your zone"
+                        :disabled="orderDisabled.orderInformation">
                         <el-option label="有线网络无法拨号" value="1" />
                         <el-option label="无线网络没有弹窗" value="2" />
                         <el-option label="不能拨号" value="22" />
@@ -19,13 +21,16 @@
                     </el-select>
                 </el-form-item>
                 <el-form-item label="创建人">
-                    <el-input v-model="orderTable.sender" placeholder="Please input" />
+                    <el-input class=input v-model="orderTable.sender" placeholder="Please input"
+                        :disabled="orderDisabled.orderInformation" />
                 </el-form-item>
                 <el-form-item label="创建时间">
-                    <el-input v-model="orderTable.timeStart" placeholder="Please input" />
+                    <el-input class=input v-model="orderTable.timeStart" placeholder="Please input"
+                        :disabled="orderDisabled.orderInformation" />
                 </el-form-item>
                 <el-form-item label="故障位置">
-                    <el-select v-model="orderTable.type" placeholder="please select your zone">
+                    <el-select class=select v-model="orderTable.type" placeholder="please select your zone"
+                        :disabled="orderDisabled.orderInformation">
                         <el-option label="其他" value="1" />
                         <el-option label="七教" value="2" />
                         <el-option label="行政楼" value="3" />
@@ -38,7 +43,8 @@
                     </el-select>
                 </el-form-item>
                 <el-form-item label="预约时间">
-                    <el-select v-model="orderTable.timeSubscribe" placeholder="please select your zone">
+                    <el-select class=select v-model="orderTable.timeSubscribe" placeholder="please select your zone"
+                        :disabled="orderDisabled.orderInformation">
                         <el-option label="12:00-12:30" value="1" />
                         <el-option label="12:30-13:00" value="2" />
                         <el-option label="08:00-08:30" value="3" />
@@ -47,7 +53,8 @@
                     </el-select>
                 </el-form-item>
                 <el-form-item label="故障描述">
-                    <el-input v-model="orderTable.des" autosize type="textarea" />
+                    <el-input class=textInput v-model="orderTable.des" autosize type="textarea"
+                        :disabled="orderDisabled.orderInformation" />
                 </el-form-item>
                 <el-form-item label="相关图片">
                     <el-space wrap>
@@ -69,13 +76,16 @@
                     </div>
                 </template>
                 <el-form-item label="创建人">
-                    <el-input v-model="orderTable.sender" placeholder="Please input" />
+                    <el-input class=input v-model="orderTable.sender" placeholder="Please input"
+                        :disabled="orderDisabled.userInformation" />
                 </el-form-item>
                 <el-form-item label="学号/工号">
-                    <el-input v-model="orderTable.username" placeholder="Please input" />
+                    <el-input class=input v-model="orderTable.username" placeholder="Please input"
+                        :disabled="orderDisabled.userInformation" />
                 </el-form-item>
                 <el-form-item label="联系方式">
-                    <el-input v-model="orderTable.tel" placeholder="Please input" />
+                    <el-input class=input v-model="orderTable.tel" placeholder="Please input"
+                        :disabled="orderDisabled.userInformation" />
                 </el-form-item>
             </el-card>
 
@@ -86,13 +96,16 @@
                     </div>
                 </template>
                 <el-form-item label="管理员工号">
-                    <el-input v-model="orderTable.solver" placeholder="Please input" />
+                    <el-input class=input v-model="orderTable.solver" placeholder="Please input"
+                        :disabled="orderDisabled.adminInformation" />
                 </el-form-item>
                 <el-form-item label="审批时间">
-                    <el-input v-model="orderTable.timeDistribution" placeholder="Please input" />
+                    <el-input class=input v-model="orderTable.timeDistribution" placeholder="Please input"
+                        :disabled="orderDisabled.adminInformation" />
                 </el-form-item>
                 <el-form-item label="管理员留言">
-                    <el-input v-model="orderTable.remark" autosize type="textarea" />
+                    <el-input class=textInput v-model="orderTable.remark" autosize type="textarea"
+                        :disabled="orderDisabled.adminInformation" />
                 </el-form-item>
             </el-card>
 
@@ -103,13 +116,16 @@
                     </div>
                 </template>
                 <el-form-item label="维修员工号">
-                    <el-input v-model="orderTable.solver" placeholder="Please input" />
+                    <el-input class=input v-model="orderTable.solver" placeholder="Please input"
+                        :disabled="orderDisabled.repairmanInformation" />
                 </el-form-item>
                 <el-form-item label="维修时间">
-                    <el-input v-model="orderTable.timeDistribution" placeholder="Please input" />
+                    <el-input class=input v-model="orderTable.timeDistribution" placeholder="Please input"
+                        :disabled="orderDisabled.repairmanInformation" />
                 </el-form-item>
                 <el-form-item label="维修员留言">
-                    <el-input v-model="orderTable.feedback" autosize type="textarea" />
+                    <el-input class=textInput v-model="orderTable.feedback" autosize type="textarea"
+                        :disabled="orderDisabled.repairmanInformation" />
                 </el-form-item>
             </el-card>
         </el-space>
@@ -121,24 +137,46 @@ import { ref, Ref } from 'vue';
 import { order } from '@/interface/order';
 import { selectOrderList, orderParam } from "@/api/order";
 import { useRoute } from 'vue-router'
-
-let orderTable: Ref<order> = ref({});
-let imgPathList = ref()
-let imgNumber = ref(0);
-
 const route = useRoute()
+
+// 图片地址列表
+let imgPathList = ref()
+// 光标选中图片号
+let imgNumber = ref(0);
+// 工单信息
+let orderTable: Ref<order> = ref({});
+// 工单块可编辑区域
+let orderDisabled = ref({
+    orderInformation: true,
+    userInformation: true,
+    adminInformation: true,
+    repairmanInformation: true
+})
+
+// 获取指定工单
 const getTableData = async (orderId: number | undefined) => {
     let params: orderParam = { orderId: orderId };
     let res = await selectOrderList(params);
     // console.log("res:", res.data[0]);
     orderTable.value = res.data[0];
+    // 解析图片地址参数
     imgPathList.value = orderTable.value.imgPath == null ? [] : orderTable.value.imgPath.split("]")[0].split("[")[1].split(",");
+    if (res.data[0].progress === 0) {
+        orderDisabled.value.adminInformation = false;
+    } else if (res.data[0].progress === 1) {
+        orderDisabled.value.repairmanInformation = false;
+    }
     // console.log("orderTable", orderTable.value);
     // console.log("imgPathList", imgPathList.value);
+    // console.log("orderDisabled", orderDisabled.value);
 };
 
 getTableData(Number(route.query.orderId));
 </script>
 
 <style lang="less" scoped>
+.input,
+.select {
+    width: 100%;
+}
 </style>
