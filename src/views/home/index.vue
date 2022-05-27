e5e8092255ab054f3fab6e1c9387f2d1682a068e
WOOGUGU <woogugu@outlook.com>
2022-05-27 13:17:41 +0800

    跳转日志

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,5 +1,9 @@
 <template>
     <div>home</div>
+    <h1>
+        <a href="https://github.com/WOOGUGU/vite-vue-ts/commits/master" target="_blank"> 修改日志
+        </a>
+    </h1>
 </template>
 
 <script setup lang="ts">
