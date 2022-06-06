29fbf489a595411f14a7aee74385228de5d0ddcf
WOOGUGU <woogugu@outlook.com>
2022-06-06 14:53:43 +0800

    修改home页面

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,13 +1,16 @@
 <template>
     <div>home</div>
     <h1>
-        <a href="https://github.com/WOOGUGU/vite-vue-ts/commits/master" target="_blank"> 提交记录 </a>
+        <!-- <a href="https://github.com/WOOGUGU/vite-vue-ts/commits/master" target="_blank"> 提交记录 </a> -->
+        <a href="https://gitee.com/woogugu/vite-vue-ts/commits/master" target="_blank"> 提交记录 </a>
     </h1>
     <h1>
-        <a href="https://github.com/WOOGUGU/vite-vue-ts/blob/master/开发进度记录.md" target="_blank"> 开发进度清单 </a>
+        <!-- <a href="https://github.com/WOOGUGU/vite-vue-ts/blob/master/开发进度记录.md" target="_blank"> 开发进度清单 </a> -->
+        <a href="https://gitee.com/woogugu/vite-vue-ts/blob/master/开发进度记录.md" target="_blank"> 开发进度清单 </a>
     </h1>
     <h1>
-        <a href="https://github.com/WOOGUGU/vite-vue-ts/blob/master/README.md" target="_blank"> 当前版本功能清单 </a>
+        <!-- <a href="https://github.com/WOOGUGU/vite-vue-ts" target="_blank"> 当前版本功能清单 </a> -->
+        <a href="https://gitee.com/woogugu/vite-vue-ts" target="_blank"> 仓库链接 </a>
     </h1>
 </template>
 
