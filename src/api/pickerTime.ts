23683de68ba1855a400e98f265ff794b630575cc
WOOGUGU <woogugu@outlook.com>
2022-09-08 16:21:43 +0800

    perf: 更换时间选择方式

--- src/api/pickerTime.ts
+++ src/api/pickerTime.ts
@@ -1,37 +1,39 @@
-import request from './request';
-import qs from 'qs'
+import request from "./request";
+import qs from "qs";
+import { ListCache } from "element-plus";
 
 export interface pickerTimeParams {
-    id?: string;
-    picker?: string;
-    pickerId?: number;
-    type?: string;
-    value?: string;
+  id?: string;
+  picker?: string;
+  pickerId?: number;
+  type?: string;
+  value?: string;
+  times?: string[];
 }
 
 // 获取报修时间段
 export const selectPickerTime = () => {
-    return request({
-        url: '/v2/picker/selectAllPickerTime',
-        method: 'get',
-    })
-}
+  return request({
+    url: "/v2/picker/selectAllPickerTime",
+    method: "get",
+  });
+};
 
 // 添加报修时间段
 export const addPickerTime = (params: pickerTimeParams) => {
-    // console.log("params", params);
-    return request({
-        url: '/v2/picker/addPicker',
-        method: 'post',
-        data: qs.stringify(params)
-    })
-}
+  // console.log("params", params);
+  return request({
+    url: "/v2/picker/addPicker",
+    method: "post",
+    data: qs.stringify(params),
+  });
+};
 
 // 删除报修时间段
 export const deletePickerTime = (params: pickerTimeParams) => {
-    return request({
-        url: '/v2/picker/deletePickerById',
-        method: 'post',
-        data: qs.stringify(params)
-    })
-}
+  return request({
+    url: "/v2/picker/deletePickerById",
+    method: "post",
+    data: qs.stringify(params),
+  });
+};
